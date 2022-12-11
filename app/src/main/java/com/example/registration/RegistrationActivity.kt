08f96968416b648380d7ba66cab955f7e250b0e4
SandroMatch <sandro.machavariani.1@bru.edu.ge>
2022-12-11 18:41:32 +0400

    123

--- app/src/main/java/com/example/registration/RegistrationActivity.kt
+++ app/src/main/java/com/example/registration/RegistrationActivity.kt
@@ -1,6 +1,5 @@
 package com.example.kai
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -8,7 +7,7 @@ import com.example.registration.R
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
-class LoginActivity : AppCompatActivity() {
+class RegistrationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
